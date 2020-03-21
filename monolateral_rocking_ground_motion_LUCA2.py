f03333100c9a411d0f6772736f8caea6bb22519d
lucagiacomelli <lucagiacomelli@bighealth.com>
2020-03-21 20:07:41 +0000

    commit opt

--- monolateral_rocking_ground_motion_LUCA2.py
+++ monolateral_rocking_ground_motion_LUCA2.py
@@ -6,6 +6,77 @@ from scipy.integrate import odeint
 import xlrd
 from scipy import interpolate
 
+
+def read_accelerations(filename):
+    """
+    Function that reads accelerations from a source file
+
+    :param filename: the name of source file
+    :return: a list of accelerations
+    """
+    # seismic action
+    wb = xlrd.open_workbook(filename)
+    s = wb.sheet_by_name("acceleration strong")
+
+    # time=np.linspace(0., tStop, int(tStop/tInc)+1)
+    # time=time[6000:10000]
+    accelerations = [s.cell_value(i, 4) * 0.01 for i in range(s.nrows)]
+
+    return accelerations
+
+
+def interpolate_time_accelerations(time_array, accelerations):
+    """
+    Function that, given equal length arrays of times and accelerations,
+    interpolate the two and generate a continuous function.
+
+    :param time_array:
+    :param accelerations:
+    :return: interpolation time-accelerations
+    """
+    assert len(time_array) == len(accelerations)
+
+    # extrapolate creates values after the last one
+    time_acceleration_interpolation = interpolate.interp1d(time_array, accelerations, fill_value="extrapolate")
+
+    return time_acceleration_interpolation
+
+
+def calculate_derivatives(y, t, p_quadro, alpha, time_acceleration_interpolation, gravity):
+    """
+    Function that scipy.odeint takes in input in order to calculate the
+    rotation theta and the angular velocity omega.
+
+    :param y: output of the differential equation
+    :param t: input of the differential equation: array of equal-spaced float
+    :param p_quadro: TODO: write here what p_quadro is
+    :param alpha: TODO: write what alpha is
+    :param time_acceleration_interpolation: interpolation object time-accelarations
+    :param gravity: gravity acceleration, i.e 9.81 m/s^2
+    :return:
+    """
+    theta, omega = y
+    # p_quadro, alpha, interpol, g = params
+
+    derivs = [omega, -p_quadro * (np.sin(alpha - theta)) + p_quadro * time_acceleration_interpolation(t) * np.cos(alpha - theta) / gravity]
+    return derivs
+
+
+def get_number_of_consecutive_negative_thetas(derivative_solution):
+    negative_theta_values = 0
+    if derivative_solution is not None:
+
+        # iterate over the negative theta values and stop when a positive is found
+        for i in range(len(derivative_solution[0][:, 0])):
+            if derivative_solution[0][i, 0] <= 0:
+                negative_theta_values += 1
+            else:
+                negative_theta_values -= 1
+                break
+    return negative_theta_values
+
+
+
 b = 0.3  # mezza base
 h = 2.4  # mezza altezza
 H = 2 * h
@@ -22,103 +93,132 @@ p_quadro = m * g * R / I
 p = np.sqrt(p_quadro)
 alpha = np.arctan(b / h)
 
+
 # Make time array for solution
 tStop = 8.000
 tInc = 0.005
 t = np.linspace(0.0, tStop, int(tStop / tInc + 1))
 
 
-# seismic action
-wb = xlrd.open_workbook("C:\\Users\\victor\\Desktop\\PHD\\rocking\\acceleration_laquila.xlsx")
-s = wb.sheet_by_name("acceleration strong")
-s.nrows
-# time=np.linspace(0., tStop, int(tStop/tInc)+1)
-# time=time[6000:10000]
-acc = [s.cell_value(i, 4) * -0.01 for i in range(s.nrows)]
-
-# extrapolate creates values after the last one
-interpol = interpolate.interp1d(t, acc, fill_value="extrapolate")
-
-
-def f(y, t, params):
-    theta, omega = y
-    p_quadro, alpha, interpol, g = params
-
-    derivs = [omega, -p_quadro * (np.sin(alpha - theta)) + p_quadro * interpol(t) * np.cos(alpha - theta) / g]
-    return derivs
-
 
 # Initial values
 theta0 = 0 * np.pi / 180  # initial angular displacement RADIANTI
 omega0 = 0.0  # initial angular velocity
-params = [p_quadro, alpha, interpol, g]
+# params = [p_quadro, alpha, interpol, g]
 y_curr = [theta0, omega0]
 
 e_1s = 1.05 * (1 - 2 * m * R ** 2 / I * np.sin(alpha) ** 2) ** 2 * (1 - 2 * m * R ** 2 / I * np.cos(alpha) ** 2)
-rotations = []
-velocities = []
+r = e_1s
+
+
+acc = read_accelerations("acceleration_laquila.xlsx")
+
+interpol = interpolate_time_accelerations(t, acc)
+
+numero_di_volte = 20
 ts = [t]
-psoln = None
-activation = 0
-
-# consider only positive rotations
-numero_di_volte = 60
-activation_acceleration = []
-y_curr_list = []
 
 
-for ndv in range(numero_di_volte):
-    rotations.append([])
-    velocities.append([])
+def find_rotations(time_array, accelerations, calculation_accuracy=0.7):
+    """
+    Functions to calculate the rotations based on the time and the accelerations.
 
-    # velocity_urto_piu=0
+    :return:
+    """
+    rotations = []
+    velocities = []
+    psoln = None
 
-    if psoln is not None:
-        if psoln[0][1, 0] > 0:
-            activation = 1
-            for i in range(len(psoln[0][:, 0])):
-                if psoln[0][i, 0] >= 0:
-                    rotations[-1] = rotations[-1] + [psoln[0][i, 0] * 180 / np.pi]
-                    velocities[-1] = velocities[-1] + [psoln[0][i, 1]]
-                    r = e_1s
-                    velocity_urto_meno = velocities[-1][-1]
-                    velocity_urto_piu = r * velocity_urto_meno
-                    # activation_acceleration+=[acc[acc_new_start]]
-                    counter = 0
-                else:
-                    break
+    activation = 0
+    current_index = 0
+
+    # the index of the time array to stop
+
+    TEST_STOP = 1600
+
+    while current_index < len(t) and current_index < TEST_STOP:
+        velocity_after_impact = 0
+
+        rotations_at_step = []
+        velocities_at_step = []
+
+        # the first time that we are here we consider velocity_impact_after == omega0
+        if psoln is None:
+            velocity_after_impact = omega0
         else:
-            activation = 0
-            velocity_urto_piu = 0
 
-    if psoln is None:
-        velocity_urto_piu = 0
+            # print(len(psoln[0]))
 
-    y_curr = [0, velocity_urto_piu]
-    y_curr_list += [y_curr]
-    if activation > 0:
-        ts.append([])
-        new_start = ts[-2][len(rotations[-1])]
-        ts[-1] = np.linspace(new_start, tStop, int(tStop / tInc + 1) - int(new_start / tInc))
-        # ts[-1] = np.arange(new_start, tStop+0.004, tInc)
-        interpol = interpolate.interp1d(ts[-1], acc[int(new_start / tInc) :], fill_value="extrapolate")
-    if activation == 0:  # non ci sono stati valori buoni nel psoln
-        ts.append([])
-        new_start = ts[-2][len(rotations[-1])] + tInc
-        ts[-1] = np.linspace(new_start, tStop, int(tStop / tInc + 1) - int(new_start / tInc))
-        # ts[-1] = np.arange(new_start + counter*tInc, tStop+0.004, tInc)
-        interpol = interpolate.interp1d(ts[-1], acc[int(new_start / tInc) :], fill_value="extrapolate")
-    print(activation, new_start)
-    params = [p_quadro, alpha, interpol, g]
-    psoln = odeint(f, y_curr, ts[-1], args=(params,), full_output=True)
+            # check the second rotation. If it is greater than zero, we consider all the
+            # positive rotations found by the calculation of the derivatives
+            if psoln[0][1, 0] > 0:
+                activation = 1
+
+                # iterate over positive theta values and stop when a negative value is found
+                for i in range(len(psoln[0][:, 0])):
+                    if psoln[0][i, 0] >= 0:
+                        current_index += 1
+
+                        rotations_at_step.append(psoln[0][i, 0] * 180 / np.pi)
+                        velocities_at_step.append(psoln[0][i, 1])
+                        velocity_urto_meno = velocities_at_step[-1]
+
+                        # the velocity after the impact is the last velocity multiplied by a coefficient
+                        velocity_after_impact = r * velocity_urto_meno
+                    else:
+                        break
+
+            else:
+                # check the new start index here
+                activation = 0
+                velocity_after_impact = 0
+
+        # rotations at_step is empty if it is the first iteration (psol is None) or
+        # the second rotation in the calculation of the derivatives is not positive
+        rotations.append(rotations_at_step)
+        velocities.append(velocities_at_step)
+
+        y_curr = [0, velocity_after_impact]
+        # ts.append([])
+
+        # "calculation accuracy" gives frequency of how many times the odeint function
+        # should be called. If the accuracy is low, all negative theta values are skipped,
+        # if it is high, no negative theta values is skipped and the odeint function is called
+        # for every interval in time.
+        # For low values of "calculation accuracy" the function is faster
+
+        negative_theta_values = get_number_of_consecutive_negative_thetas(psoln)
+
+        # we skip all the negative thetas
+        if activation == 0:
+            current_index += int(negative_theta_values * (1-calculation_accuracy)) + 1
+
+        print(activation, current_index)
+
+        next_time_array = ts[0][current_index:]
+        ts.append(next_time_array)
+
+        # for interpolation we need at least two entries
+        if len(next_time_array) < 2:
+            return rotations
+        else:
+            interpol = interpolate_time_accelerations(next_time_array, acc[current_index:])
+            psoln = odeint(calculate_derivatives, y_curr, next_time_array, args=(p_quadro, alpha, interpol, g), full_output=True)
+
+    return rotations
+
+
+rotations = find_rotations(ts, acc)
+
 
 # plot all together
-
 t_total = []
 rotation_total = []
-for ndv in range(numero_di_volte):
+for ndv in range(len(rotations)):
     t_total += list(ts[ndv][: len(rotations[ndv])])
     rotation_total += rotations[ndv]
+
+
 initial_part_time = list(np.linspace(0.0, t_total[0], int(t_total[0] / tInc + 1)))
 initial_part_rotations = [0] * len(initial_part_time)
 
@@ -151,3 +251,6 @@ ax2.set_ylabel("seismic acceleration", color="blue")  # we already handled the x
 ax2.plot(t_total, interpol(t_total))
 ax2.plot(t_total, [0] * len(t_total), "b")
 ax2.tick_params(axis="y", labelcolor="blue")
+
+
+plt.show()
